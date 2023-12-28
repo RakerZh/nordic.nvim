25f8b389a39d52dc7d29784a23f52fc28db35795
RakerZh <gitrustz10@gmail.com>
2023-12-28 00:33:54 -0500

    update

---
+++ lua/nordic/groups/integrations/epo.lua
@@ -0,0 +1,9 @@
+local C = require 'nordic.colors'
+
+
+return {
+    PmenuExtra = { fg = C.blue2, bg = C.green.base },
+    PmenuKind = { fg = C.blue3 },
+    PmenuKindSel = { fg = C.blue1, bold = true },
+    PmenuExtraSel = { fg = C.blue2 },
+}
